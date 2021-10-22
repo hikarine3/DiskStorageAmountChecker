f7bc9e9eebaa4003bec5d85ba76f18aca814c21e
Hajime Kurita <hajimekurita@1stclass.co.jp>
2021-10-22 17:52:59 +0900

    added initial files

---
+++ DiskStorageAmountChecker.py
@@ -0,0 +1,107 @@
+import os
+import re
+from firstclass_dotenv import Dotenv
+import smtplib
+import subprocess
+from email.header import Header
+from email.mime.text import MIMEText
+from email import charset
+from pprint import pprint
+
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+class DiskStorageAmountChecker():
+
+    def help(self):
+        print("Example:")
+        print("python3 DiskStorageAmountChecker.py --email=aaa@example.com --servers=server1,server2 --alert=70 --user=ssh_user_id")
+        sys.exit(0)
+
+    def __init__(self):
+        env_file = '../area/.env'
+        self.alert = str(50)
+        if(not os.path.exists(env_file)):
+            print(env_file + "doesn't exist.")
+            sys.exit()
+        dotenv = Dotenv()
+        dotenv.load(env_file)
+        self.smtp_server = os.environ['MAIL_HOST']
+        self.smtp_port = os.environ['MAIL_PORT']
+        self.smtp_user = os.environ['MAIL_USERNAME']
+        self.smtp_password = os.environ['MAIL_PASSWORD']
+        self.from_email = os.environ['MAIL_FROM_ADDRESS']
+        self.servers = []
+        self.user = ''
+        self.result = ''
+        self.debug = 0
+
+        for argv in sys.argv[1:]:
+            matches = []
+            if re.search('^--', argv):
+                if argv == '--debug':
+                    self.debug += 1
+                elif re.search('=', argv):
+                    matches = re.findall( r'^--([^=]+)=([^=]+)$', argv, re.IGNORECASE | re.DOTALL)
+                    name = matches[0][0]
+                    value = matches[0][1]
+                    if name == 'alert':
+                        self.alert = value
+                    elif name == 'email':
+                        self.email = value
+                    elif name == 'user':
+                        self.user = value
+                    elif name == 'servers':
+                        self.servers = value.split(',')
+        if self.email == "":
+            print("please input --email=...")
+            sys.exit()
+
+    def check(self):
+        for server in self.servers:
+            cm = 'ssh -l ' + self.user + ' ' + server +' "df -kh" | perl -e ' +"'" + 'while(<>){$_=~ s{(\d+)\%}{if($1>=' + self.alert + '){print("' + server + '"."\t".$_);}}e;}'+"'"
+            print(cm)
+            result = subprocess.check_output([cm], shell=True).decode("UTF-8")
+            if result:
+                self.result += result
+    
+    def report(self):
+        if self.result:
+            subject = 'alert: disk full is near'
+            self.send({'body':self.result, 'from':self.from_email, 'to':self.email, 'subject':subject})
+
+    def send(self, op):
+        cset = 'utf8'
+        msg = MIMEText(op['body'], 'plain', cset)
+        msg['Subject'] = Header(op['subject'], cset)
+        msg['From'] = op['from']
+        msg['To'] = op['to']
+
+        try:
+            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            if self.debug:
+                server.set_debuglevel(1)
+            if self.smtp_user and self.smtp_password:
+                server.starttls()
+                server.login(self.smtp_user, self.smtp_password)
+            server.sendmail(op['from'], [op['to']], msg.as_string())
+            server.quit()
+            print("Sent email")
+        except:
+            print("Failed to send email / ")
+            print(sys.exc_info()[0])
+            pprint(op)
+            print(self.smtp_server)
+            print(self.smtp_port)
+            print(self.smtp_user)
+            print(self.smtp_password)
+            sys.exit(1)
+
+    def run(self):
+        self.check()
+        self.report()
+
+if __name__ == '__main__':
+    pro = CheckDisks()
+    pro.run()
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # DiskStorageAmountChecker
-check disk storage's amount and if necessary, send alert message by email
+This script heck disk storage's amount of specified servers and send alerting message by email if necessary.
+
+For it, you have to configure .env file to set up smtp's usage.
+
+Command example
+```
+python3 DiskStorageAmountChecker.py --email=aaa@example.com --servers=server1,server2 --alert=70 --user=ssh_user_id
+```
+
+If you want to check only disk which is near disk full through shell command,
+
+```
+#!/bin/sh
+user=UserId; # please change
+alert_per=75; # please adjust
+servers=('server1' 'server2') # please change
+for server in ${servers[@]}
+do
+        ssh -l $user $server 'df -kh' | perl -e 'while(<>){$_=~ s{(\d+)\%}{if($1>='$alert_per'){print("'$server'"."\t".$_);}}e;}'
+done
+```
+is enough
+
+
+# License / ライセンス / 执照
+
+MIT
+
+# Author / 作者
+
+## Name / 名前 / 全名
+Hajime Kurita
+
+## Twitter
+- EN: https://twitter.com/hajimekurita
+- JP: https://twitter.com/hikarine3
+
+## Technical web services / 提供してる技術関連Webサービス / 技术网络服务
+### VPS & Infra comparison / VPS比較 / VPS比较
+- EN: https://vpsranking.com/en/
+- CN: https://vpsranking.com/zh/
+- JP: https://vpshikaku.com/
+
+### Programming Language Comparison / プログラミング言語比較 / 编程语言比较
+- EN: https://programminglang.com/en/
+- CN: https://programminglang.com/zh/
+- JP: https://programminglang.com/ja/
+
+### OSS
+- Docker: https://hub.docker.com/u/1stclass/
+- Github: https://github.com/hikarine3
+- NPM: https://www.npmjs.com/~hikarine3
+- Perl: http://search.cpan.org/~hikarine/
+- PHP: https://packagist.org/packages/hikarine3/
+- Python: https://pypi.org/user/hikarine3/
